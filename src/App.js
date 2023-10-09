e4890cb305a8f9b1e48327173598f29c6b02e13b
jagadeesh <jagadeesh.1724@gmail.com>
2023-10-09 06:44:53 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import {Route, Switch, Redirect} from 'react-router-dom'
+
+import {Component} from 'react'
+
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
 import AllJobs from './components/AllJobs'
-// import ProtectedRoute from './components/ProtectedRoute'
 import AboutJobItem from './components/AboutJobItem'
 import ApplicationForm from './components/ApplicationForm'
+import ApplicationSuccess from './components/ApplicationSuccess'
+
+import CreateContext from './Context/CreatContext'
 
 import './App.css'
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={LoginForm} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/jobs" component={AllJobs} />
-    <Route exact path="/jobs/:id" component={AboutJobItem} />
-    <Route exact path="/application-form" component={ApplicationForm} />
-    <Route path="/not-found" component={NotFound} />
-    <Redirect to="not-found" />
-  </Switch>
-)
+class App extends Component {
+  state = {
+    submit: true,
+  }
+
+  toggleSubmit = () => {
+    this.setState(prevState => ({submit: !prevState.submit}))
+  }
+
+  render() {
+    const {submit} = this.state
+    return (
+      <CreateContext.Provider
+        value={{
+          submit,
+          toggleSubmit: this.toggleSubmit,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/jobs" component={AllJobs} />
+          <Route exact path="/jobs/:id" component={AboutJobItem} />
+          <Route exact path="/application-form" component={ApplicationForm} />
+          <Route
+            exact
+            path="/application-success"
+            component={ApplicationSuccess}
+          />
+
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CreateContext.Provider>
+    )
+  }
+}
 
 export default App
 
--- src/components/ApplicationForm/index.js
+++ src/components/ApplicationForm/index.js
@@ -1,21 +1,155 @@
 import {Component} from 'react'
+import {Link} from 'react-router-dom'
+
+import {BiUpload} from 'react-icons/bi'
+
+import CreateContext from '../../Context/CreatContext'
 
 import Header from '../Header'
 
 import './index.css'
 
 class ApplicationForm extends Component {
-  renderApplicationForm = () => (
-    <div className="application-form-container">
-      <h1 className="application-form-hd">Application Form</h1>
-    </div>
+  state = {
+    nameInput: '',
+    name: '',
+    emailInput: '',
+    email: '',
+    coverLetterInput: '',
+    coverLetter: '',
+  }
+
+  onClickNameInput = event => {
+    this.setState({nameInput: event.target.value})
+  }
+
+  onClickEmailInput = event => {
+    this.setState({emailInput: event.target.value})
+  }
+
+  onClickCvInput = event => {
+    this.setState({coverLetterInput: event.target.value})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {nameInput, emailInput, coverLetterInput} = this.state
+
+    this.setState({
+      name: nameInput,
+      nameInput: '',
+      email: emailInput,
+      emailInput: '',
+      coverLetter: coverLetterInput,
+      coverLetterInput: '',
+    })
+  }
+
+  renderApplicationForm = () => {
+    const {nameInput, emailInput, coverLetterInput} = this.state
+
+    return (
+      <>
+        <form className="form-container" onSubmit={this.onSubmitForm}>
+          <label htmlFor="name" className="label">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="input"
+            value={nameInput}
+            onChange={this.onClickNameInput}
+          />
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="input"
+            value={emailInput}
+            onChange={this.onClickEmailInput}
+          />
+          <label htmlFor="cover-letter" className="label">
+            Cover Letter
+          </label>
+          <textarea
+            id="cover-letter"
+            className="input text-area"
+            onChange={this.onClickCvInput}
+            value={coverLetterInput}
+          />
+          <div className="upload-box-container">
+            <BiUpload className="upload-icon" />
+            <p className="upload-cover-desc">Upload cover image</p>
+            <p className="upload-cover-details">
+              16:9 ratio is recommended. Max image size 1mb
+            </p>
+          </div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </>
+    )
+  }
+
+  renderApplicationDetails = () => {
+    const {name, email, coverLetter} = this.state
+
+    return (
+      <div className="details-view-container">
+        <h1>{name}</h1>
+        <p>{email}</p>
+        <p>{coverLetter}</p>
+      </div>
+    )
+  }
+
+  renderSubmit = () => (
+    <CreateContext.Consumer>
+      {value => {
+        const {submit, toggleSubmit} = value
+
+        const onClickSubmitBtn = () => {
+          toggleSubmit()
+        }
+
+        return (
+          <>
+            {submit ? (
+              <button
+                type="button"
+                className="submit-btn"
+                onClick={onClickSubmitBtn}
+              >
+                Submit
+              </button>
+            ) : (
+              <Link to="/application-success" className="link-check">
+                Check out
+              </Link>
+            )}
+          </>
+        )
+      }}
+    </CreateContext.Consumer>
   )
 
   render() {
     return (
       <>
         <Header />
-        {this.renderApplicationForm()}
+
+        <div className="application-form-container">
+          <h1 className="application-form-hd">Application Form</h1>
+          <div className="application-details-container">
+            {this.renderApplicationForm()}
+            <div>{this.renderApplicationDetails()}</div>
+          </div>
+          {this.renderSubmit()}
+        </div>
       </>
     )
   }
